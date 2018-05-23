08aa2983a8533ca6629e437bbbe4dc405d585efc
Ken Moynihan <ken@kenmoynihan.com>
2018-05-23 10:06:37 +0800

    Initial Commit

---
+++ testUIprogrammatically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testUIprogrammatically
+//
+//  Created by Ken Moynihan on 23/05/18.
+//  Copyright © 2018 Ken Moynihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
